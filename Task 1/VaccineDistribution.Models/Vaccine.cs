eada0f1d83a71b4047a683ffb8f3ae90eb2e1e97
Isuru Sathsara <Isuru.Sa@CreativeSoftware.com>
2021-11-04 11:51:07 +0530

    'Blazor-Task'

---
+++ Task 1/VaccineDistribution.Models/Vaccine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace VaccineDistribution.Models
+{
+    public class Vaccine
+    {
+        public string VaccineName { get; set; }
+        public int Noofdoses { get; set; }
+    }
+}
---
+++ Task 1/Vaccinedetails/Vaccineinfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vaccinedetails
+{
+    public class Vaccineinfo
+    {
+        public int BatchNo { get; set; }
+        public string Name { get; set; }
+        public string distribution { get; set; }
+        public int NoofDoses { get; set; }
+
+    }
+}
