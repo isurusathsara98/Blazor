1eef1a95477594d795a43ef721190d7334f19eb5
Isuru Sathsara <Isuru.Sa@CreativeSoftware.com>
2021-11-08 21:23:25 +0530

    Vaccine_info

---
+++ Vaccines/Vaccines/Server/Controllers/VaccineController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vaccines.Shared;
+
+namespace Vaccines.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VaccineController : ControllerBase
+    {
+        static List<VaccineInfo> vaccine = new List<VaccineInfo>
+        {
+            new VaccineInfo
+            {
+                Id=1,
+                Name="Sinopharm",
+                BatchNo=200,
+                Area="Galle",
+                NoofDoses=14000
+            },
+            new VaccineInfo
+            {
+                Id=2,
+                Name="Astraseni",
+                BatchNo=400,
+                Area="Gampaha",
+                NoofDoses=20000
+            },
+
+        };
+
+        [HttpGet]
+        public async Task<IActionResult> GetVaccine()
+        {
+            return Ok(vaccine);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Createvac(VaccineInfo vac)
+        {
+            vac.Id = vaccine.Max(h => h.Id + 1);
+            vaccine.Add(vac);
+            return Ok(vaccine);
+        }
+    }
+}
---
+++ Vaccines/Vaccines/Shared/VaccineInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaccines.Shared
+{
+    public class VaccineInfo
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public int BatchNo { get; set; }
+        public string Area { get; set; }
+
+        public int NoofDoses { get; set; }
+    }
+}
